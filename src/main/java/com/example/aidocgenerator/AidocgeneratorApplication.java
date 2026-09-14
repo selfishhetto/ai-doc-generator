29310b23d0d9b5dba4691f6025325b653fc4fb4f
selfishhetto <gektorahilll@gmail.com>
2026-09-14 20:47:01 +0000

    init Spring Boot skeleton generated by Initializer

---
+++ src/main/java/com/example/aidocgenerator/AidocgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.aidocgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidocgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AidocgeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aidocgenerator/AidocgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aidocgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidocgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
